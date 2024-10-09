31ba345f35e27fd0d07ee4e76dfe3f937781c70e
andS00 <andrea02micro@outlook.it>
2024-10-09 15:57:12 +0200

    main nuovomenu

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ WIDTH, HEIGHT = 800, 600
 class Personaggio(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.image.load('image/ship.png')
-        self.image = pygame.transform.scale(self.image, (125,75))
+        self.image = pygame.image.load('image/aliennave.png')
+        self.image = pygame.transform.scale(self.image, (100,100))
 
         self.rect = self.image.get_rect()
 
@@ -33,35 +33,31 @@ class Personaggio(pygame.sprite.Sprite):
         self.velocita_x += x
         self.velocita_y += y
 
-
-personaggio = Personaggio(100, 100)
-
+        
+personaggio = Personaggio(50, 250)
 gruppo_di_personaggi = pygame.sprite.Group()
 gruppo_di_personaggi.add(personaggio)
 
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Alien Revenge")
-bg = pygame.image.load("image/Space.jpg")
+bg = pygame.image.load("image/nuovospace.png")
 bgX = 0
 bgX2 = bg.get_width()
+menubg = pygame.image.load("image/alienisolation.jpg")
+menubg = pygame.transform.scale(menubg, (800, 600))
 
 orologio = pygame.time.Clock()
 frame_rate = 60
 speedScrl = 4
 game_state = "start_menu"
 
-def draw_start_menu():
-   screen.fill((0, 0, 0))
-   font = pygame.font.SysFont(None, 40)
-   font2 = pygame.font.SysFont(None, 100)
-   font3 = pygame.font.SysFont(None, 40)
-   title = font2.render('Alien Revenge', True, (255, 255, 255))
-   start_button = font.render('>>Premi Spazio<<', True, (255, 255, 255))
-   change_ship = font3.render('Seleziona Nave', True, (255, 255, 255))
-   screen.blit(title, (150, 100))
-   screen.blit(start_button, (260, 400))
-   screen.blit(change_ship, (280, 300))
-   pygame.display.update()
+font = pygame.font.Font(None, 25)
+font2 = pygame.font.Font(None, 100)
+title = font2.render('Alien Revenge', True, (255, 0, 0))
+button_surface = pygame.Surface((150, 50))
+text = font.render(">>Click to Start<<", True, (0, 0, 0))
+text_rect = text.get_rect(center=(button_surface.get_width()/2, button_surface.get_height()/2))
+button_rect = pygame.Rect(315, 400, 150, 50) 
 
 
 while True:
@@ -78,7 +74,7 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-        #Comandi Navicella
+         #Comandi Navicella
             if game_state == "game":   
                 if event.key == pygame.K_a:
                     personaggio.cambia_velocita(-5, 0)
@@ -101,10 +97,23 @@ while True:
 
     #Menu
     if game_state == "start_menu":
-        draw_start_menu()
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE]:
-            game_state = "game"
+      if event.type == pygame.MOUSEBUTTONDOWN:
+       if button_rect.collidepoint(event.pos):
+          game_state = "game"
+      if button_rect.collidepoint(pygame.mouse.get_pos()):
+        pygame.draw.rect(button_surface, (255, 0, 0), (1, 1, 148, 48))
+      else:
+       pygame.draw.rect(button_surface, (0, 0, 0), (0, 0, 150, 50))
+       pygame.draw.rect(button_surface, (255, 255, 255), (1, 1, 148, 48))
+       pygame.draw.rect(button_surface, (0, 0, 0), (1, 1, 148, 1), 2)
+       pygame.draw.rect(button_surface, (0, 0, 0), (1, 48, 148, 10), 2)    
+
+      screen.blit(menubg, (0, 0))
+      screen.blit(title, (150, 100))
+      button_surface.blit(text, text_rect)
+      screen.blit(button_surface, (button_rect.x, button_rect.y))
+
+      pygame.display.update()
     
     #Gioco
     elif game_state == "game":
@@ -117,10 +126,10 @@ while True:
             
         if bgX2 < bg.get_width() * -1:
             bgX2 = bg.get_width()
-    
+
         screen.blit(bg, (bgX, 0))  
         screen.blit(bg, (bgX2, 0))  
         gruppo_di_personaggi.update()
         gruppo_di_personaggi.draw(screen)
         
-        pygame.display.update()     
+        pygame.display.update()       
